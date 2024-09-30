268582ffb507cc741181fc148dd1400d4a264767
davit dvalashvili <davitdvalashvili1996@gmail.com>
2024-09-30 21:19:43 +0400

    feat: add routes with react router dom

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,29 @@
+import {
+  RouterProvider,
+  createBrowserRouter,
+  Route,
+  createRoutesFromElements,
+  Navigate,
+} from "react-router-dom";
+import Home from "./pages/Home";
+import Users from "./pages/Users";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/">
+      <Route index element={<Home />} />
+      <Route path="users" element={<Users />} />
+      <Route path="*" element={<Navigate to="/" replace />} />
+    </Route>
+  )
+);
+
 function App() {
-  return <main className="bg-red-500 text-[200px] font-bpg ">მთავრული</main>;
+  return (
+    <>
+      <RouterProvider router={router} />
+    </>
+  );
 }
 
 export default App;
---
+++ client/src/pages/Users.tsx
@@ -0,0 +1,5 @@
+const Users = () => {
+  return <div>usersList</div>;
+};
+
+export default Users;
